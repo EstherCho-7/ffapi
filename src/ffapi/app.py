85b779abf1d7293c9f071e02b9b1d26156c9bccc
EstherCho-7 <dkgkqm777@gmail.com>
2024-08-13 10:23:18 +0900

    replace null

--- src/ffapi/app.py
+++ src/ffapi/app.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI, HTTPException
 import pandas as pd
+import requests #(req() 만들기 위함)
 
 app = FastAPI()
 
@@ -11,6 +12,22 @@ df=pd.read_parquet("/home/esthercho/code/ffapi/data")
 def read_root():
     return {"Hello": "World"}
 
+def gen_url(movieCd="20156962"):
+    base_url="http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json"
+    import os
+    key=os.getenv("MOVIE_API_KEY")
+    url=f"{base_url}?key={key}&movieCd={movieCd}"
+    for k, v in url_param.items():
+        url=url+f"&{k}={v}"
+    return url
+
+def req(movie_cd="20156962"):
+    url=gen_url(movie_cd)
+    r=requests.get(url)
+    code=r.status_code
+    data=r.json()
+    print(url)
+    return code, data
 
 @app.get("/sample")
 def sample_data(movie_cd: int):
@@ -31,6 +48,9 @@ def movie_meta(movie_cd: str):
         raise HTTPException(status_code=404, detail="영화를 찾을 수 없습니다.")
     
     r = meta_df.iloc[0].to_dict()
+    if r['repNationCd'] is None:
+        rr=req(nationNm)
+        r['repNationCd']=rr['nationNm']
 
     # df에서 movieCd==movie_cd row를 dictionary로 df[['a']==b]...
     # 조회된 데이터를 .to_dict()로 만들어 아래에서 return
