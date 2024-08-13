bb867303ad34deaef070694c1744fb0ed7b68d1a
EstherCho-7 <dkgkqm777@gmail.com>
2024-08-13 11:00:29 +0900

    replace null

--- src/ffapi/app.py
+++ src/ffapi/app.py
@@ -17,42 +17,34 @@ def gen_url(movieCd="20156962"):
     import os
     key=os.getenv("MOVIE_API_KEY")
     url=f"{base_url}?key={key}&movieCd={movieCd}"
-    for k, v in url_param.items():
-        url=url+f"&{k}={v}"
+#    for k, v in url_param.items():
+#        url=url+f"&{k}={v}"
     return url
 
 def req(movie_cd="20156962"):
     url=gen_url(movie_cd)
     r=requests.get(url)
-    code=r.status_code
     data=r.json()
-    print(url)
-    return code, data
+    nations=data['movieInfoResult']['movieInfo']['nations']
+    nationNm = nations[0]['nationNm'] if nations else '정보 없음'
+
+    return nationNm
 
 @app.get("/sample")
 def sample_data(movie_cd: int):
-#    df=pd.read_parquet("/home/esthercho/code/ffapi/data")
     sample_df=df.sample(n=5)
     r=sample_df.to_dict(orient='records')
     
-    # df에서 movieCd==movie_cd row를 dictionary로 df[['a']==b]...
-    # 조회된 데이터를 .to_dict()로 만들어 아래에서 return
-
     return r
 
 @app.get("/movie/{movie_cd}")
 def movie_meta(movie_cd: str):
-#    df=pd.read_parquet("/home/esthercho/code/ffapi/data")
     meta_df = df[df['movieCd']== movie_cd]
     if meta_df.empty:
         raise HTTPException(status_code=404, detail="영화를 찾을 수 없습니다.")
     
     r = meta_df.iloc[0].to_dict()
     if r['repNationCd'] is None:
-        rr=req(nationNm)
-        r['repNationCd']=rr['nationNm']
-
-    # df에서 movieCd==movie_cd row를 dictionary로 df[['a']==b]...
-    # 조회된 데이터를 .to_dict()로 만들어 아래에서 return
+        r['repNationCd']=req(movie_cd)
 
     return r
